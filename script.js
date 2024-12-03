3b6fce86cfb032ea314a3dbd4f1ffde417fd22e1
Kiryl Shtepa <originalman734@gmail.com>
2024-12-03 13:03:19 +0300

    Урок 20 Логические операторы

--- script.js
+++ script.js
@@ -93,3 +93,48 @@ switch (num) {
         console.log('Не в этот раз!');
         break;
 }*/
+
+
+
+
+
+
+
+
+//===========================УРОК 20 ЛОГИЧЕСКИЕ ОПЕРАТОРЫ==============================
+
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//     console.log('Я сыт!');
+// }
+
+// console.log((hamburger && fries));
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'jkhdkjqhkdjh');
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Все сыты!');
+// } else {
+//     console.log('Мы уходим');
+// }
+
+const hamburger = 3;
+const fries = 0;
+const cola = 0;
+
+if (hamburger || cola || fries) {
+    console.log('Все довольны!');
+} else {
+    console.log('Мы уходим');
+}
